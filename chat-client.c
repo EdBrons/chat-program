144146811cf55767539a6b938f19d11f62fe848a
Ian Brons <ianebrons@gmail.com>
2021-11-22 14:26:05 -0500

    added stdin handling for client

--- chat-client.c
+++ chat-client.c
@@ -10,17 +10,38 @@
 #include <stdio.h>
 #include <netdb.h>
 #include <string.h>
+#include <pthread.h>
 
 #define BUF_SIZE 4096
+
+void *handle_io(void *arg) {
+    int conn_fd;
+    char buf[BUF_SIZE] = { 0 };
+    memcpy(&conn_fd, arg, sizeof(int));
+    while (fgets(buf, BUF_SIZE, stdin)) {
+        /* uncomment this to remove newlines from buf */
+        // buf[strcspn(buf, "\n")] = '\0';
+        puts(buf);
+        memset(buf, 0, BUF_SIZE);
+    }
+    return NULL;
+}
+
+void *handle_conn(void *arg) {
+    int conn_fd;
+    memcpy(&conn_fd, arg, sizeof(int));
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     char *dest_hostname, *dest_port;
     struct addrinfo hints, *res;
     int conn_fd;
-    char buf[BUF_SIZE];
-    int n;
     int rc;
+
+    pthread_t io_thread;
+    pthread_t conn_thread;
 
     dest_hostname = argv[1];
     dest_port     = argv[2];
@@ -47,8 +68,17 @@ int main(int argc, char *argv[])
 
     printf("Connected\n");
 
+    pthread_create(&io_thread, NULL, handle_io, &conn_fd);
+    pthread_create(&conn_thread, NULL, handle_conn, &conn_fd);
+
+    pthread_join(io_thread, NULL);
+    pthread_join(conn_thread, NULL);
+
+    close(conn_fd);
+
     /* infinite loop of reading from terminal, sending the data, and printing
      * what we get back */
+    /*
     while((n = read(0, buf, BUF_SIZE)) > 0) {
         send(conn_fd, buf, n, 0);
 
@@ -56,8 +86,7 @@ int main(int argc, char *argv[])
         printf("received: ");
         puts(buf);
     }
-
-    close(conn_fd);
+    */
 }
 
 
